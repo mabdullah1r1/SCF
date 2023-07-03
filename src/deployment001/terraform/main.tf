7a5202a066475154bd67debf29c1733b33b82667
mabdullah1r1 <138159658+mabdullah1r1@users.noreply.github.com>
2023-07-03 12:52:30 +0530

    Update main.tf

--- src/deployment001/terraform/main.tf
+++ src/deployment001/terraform/main.tf
@@ -1,8 +1,20 @@
+terraform {
+  backend "local" {
+    path = "terraform"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "my01-resource01-group01"
+  name     = "github-rg-00002"
   location = "West Europe"
 }
