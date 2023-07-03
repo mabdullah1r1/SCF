a09ec8768cfa758aca80c89357c9a0e296c4f967
mabdullah1r1 <138159658+mabdullah1r1@users.noreply.github.com>
2023-07-03 12:54:57 +0530

    Update and rename main.tf.txt to main.tf

---
+++ src/deployment006/terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "local" {
+    path = "terraform"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "github-rg-0007"
+  location = "West Europe"
+}
