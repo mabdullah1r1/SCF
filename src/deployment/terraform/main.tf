eba0fce9d75ed8462d1db86eac4c6f3c9b2c2f4b
mabdullah1r1 <138159658+mabdullah1r1@users.noreply.github.com>
2023-07-05 16:48:52 +0530

    Update main.tf

--- src/deployment/terraform/main.tf
+++ src/deployment/terraform/main.tf
@@ -14,7 +14,16 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_pet" "rg_name" {
+  length    = 2
+  separator = "-"
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "github-rg01-0001"
-  location = "West Europe"
+  name     = random_pet.rg_name.id
+  location = "eastus2"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
 }
